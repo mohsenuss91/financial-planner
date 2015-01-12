598c1cde255b4e1fa865e3312f00a2311b445ad7
Richard Lu <richard.wh.lu@gmail.com>
2015-01-12 13:47:33 -0800

    start skeleton file

---
+++ skeleton.py
@@ -0,0 +1,20 @@
+"""
+A simple and minimalistic data analytics tool for personal finance.
+
+Olivia Wung
+Richard Lu
+1.12.14
+
+"""
+
+class Finance_Tool:
+
+
+	"""should pull and store previous data here"""
+	def __init__(self):
+		//FIXME
+
+
+
+
+
