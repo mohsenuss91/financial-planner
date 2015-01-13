399a683863264c0fc069f47dac7ea3f0a9f60b2e
Richard Lu <richard.wh.lu@gmail.com>
2015-01-12 18:19:54 -0800

    create basic gui functionality, with input range for data

--- skeleton.py
+++ skeleton.py
@@ -6,14 +6,100 @@ Richard Lu
 1.12.14
 
 """
+import pandas
+import csv
+import sys
+from PyQt4 import QtGui, QtCore
+from operator import itemgetter
 
-class Finance_Tool:
 
+
+
+
+class FinanceTool(QtGui.QMainWindow):
 
 	"""should pull and store previous data here"""
 	def __init__(self):
-		//FIXME
+		# //FIXME
 
+		# inheriting from parent class qtgui
+		super(FinanceTool, self).__init__()
+
+		self.initUI()
+
+		# # reads in data
+		# self.aList = []
+		# f = open("/////.csv", "rb")
+		# reader = csv.reader(f)
+		# for row in reader:
+		# 	self.aList.append(row)
+		# f.close()
+
+
+		# # to sort data
+		# this = sorted(aList, key=itemgetter(0, 2))
+
+	def initUI(self):
+
+		self.start = QtGui.QPushButton("Start Date", self)
+		self.start.move(100, 100)
+		self.start.clicked.connect(self.inputRange)
+
+		self.end = QtGui.QPushButton("End Date", self)
+		self.end.move(100, 300)
+		self.end.clicked.connect(self.inputRange)
+
+		self.setGeometry(300, 170, 700, 400)
+		self.setWindowTitle("Personal Finance Tool")
+		self.show()
+
+	def inputRange(self):
+
+		self.check = self.sender()
+		self.popup = Calendar(self.check)
+
+		# need to figure out how to extract the startDate
+
+class Calendar(QtGui.QMainWindow):
+
+	def __init__(self, check):
+
+		super(Calendar, self).__init__()
+		self.check = check
+		self.initUI()
+
+	def initUI(self):
+
+		self.calendar = QtGui.QCalendarWidget(self)
+		self.calendar.setGridVisible(True)
+		self.calendar.clicked[QtCore.QDate].connect(self.saveDate)
+
+		self.setGeometry(300, 300, 300, 300)
+		self.setWindowTitle("Select a Date")
+		self.show()
+
+	def saveDate(self):
+		self.date = self.calendar.selectedDate()
+		if self.check.text() == "Start Date":
+			self.startDate = self.date
+		else:
+			self.endDate = self.date
+
+		print self.startDate
+		# need to put this date into a box 
+		QtCore.QCoreApplication.instance().quit()
+
+
+
+
+# runs the script
+def main():
+	app = QtGui.QApplication(sys.argv)
+	ex = FinanceTool()
+	sys.exit(app.exec_())
+
+if __name__ == "__main__":
+	main()
 
 
 
